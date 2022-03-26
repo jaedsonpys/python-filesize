3109087209073b1e2038410c5d0e45968e52af98
Jaedson Silva <contacanal798@gmail.com>
2022-03-25 22:30:48 -0300

    main: adding arguments to choose storage unit of measure

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from typing import BinaryIO
 
 
-def get_file_size(file: BinaryIO) -> tuple:
+def get_file_size(
+    file: BinaryIO,
+    kb: bool = None,
+    mb: bool = None,
+    gb: bool = None
+) -> float:
     if not isinstance(file, BinaryIO):
         raise Exception('The file must be opened in binary mode')
 
