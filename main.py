35d4fd2a945328269c333e58378c8c971e233bf7
Jaedson Silva <contacanal798@gmail.com>
2022-03-25 22:41:34 -0300

    main: changing type of "file" argument

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from typing import BinaryIO
+from io import BufferedReader
 
 
 def get_file_size(
-    file: BinaryIO,
+    file: BufferedReader,
     kb: bool = None,
     mb: bool = None,
     gb: bool = None
 ) -> float:
-    if not isinstance(file, BinaryIO):
+    print(type(file))
+    if not isinstance(file, BufferedReader):
         raise Exception('The file must be opened in binary mode')
 
     file_content = file.read()
